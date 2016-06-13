2d8d0d15b48ed463c9fa0f1f8e86306e23a6a2d9
Adrian Zniszczyński <adrian@zniszczynski.pl>
2016-06-13 14:19:02 +0200

    pug added; debugging scopes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var dashboard = require('./routes/dashboard');
+var signup = require('./routes/signup');
 
 var port = process.env.PORT || 3000;
 
@@ -28,7 +29,7 @@ var monk = require('monk');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -57,6 +58,7 @@ app.get('*', function(req, res, next) {
 });
 
 app.use('/', routes);
+app.use('/signup', signup);
 app.use('/dashboard', dashboard);
 
 // catch 404 and forward to error handler
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,13 +2,47 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
-router.get('/', isLoggedIn, function(req, res) {
-  res.render('dashboard');
-});
+var object = {
+  name: "Przykładowa lekcja ",
+  description: "Przykładowa notka",
+  roots: [
+    {
+      id: 1,
+      name: "Przykładowy korzeń"
+    }
+  ],
+  childs: [
+    {
+      id: 10,
+      name: "Przykładowe dziecko",
+      roots: [0,2,3],
+      childs: [1,2,3]
+    },
+    {
+      id: 11,
+      name: "Przykładowe dziecko",
+      roots: [0],
+      childs: [1,2,3]
+    },
+    {
+      id: 12,
+      name: "Przykładowe dziecko",
+      roots: [0],
+      childs: [1,2,3]
+    },
+    {
+      id: 13,
+      name: "Przykładowe dziecko",
+      roots: [0],
+      childs: [1,2,3]
+    }
+  ]
+};
 
-router.get('/:id', isLoggedIn, function(req, res) {
-  var object = {
-    name: "Przykładowa lekcja "+req.params.id,
+router.get('/', isLoggedIn, function(req, res) {
+  var id = 0;
+  res.render('dashboard', {
+    name: "Przykładowa lekcja ",
     description: "Przykładowa notka",
     roots: [
       {
@@ -19,22 +53,34 @@ router.get('/:id', isLoggedIn, function(req, res) {
     childs: [
       {
         id: 10,
-        name: "Przykładowe dziecko"
+        name: "Przykładowe dziecko",
+        roots: [0,2,3],
+        childs: [1,2,3]
       },
       {
         id: 11,
-        name: "Przykładowe dziecko"
+        name: "Przykładowe dziecko",
+        roots: [0],
+        childs: [1,2,3]
       },
       {
         id: 12,
-        name: "Przykładowe dziecko"
+        name: "Przykładowe dziecko",
+        roots: [0],
+        childs: [1,2,3]
       },
       {
         id: 13,
-        name: "Przykładowe dziecko"
+        name: "Przykładowe dziecko",
+        roots: [0],
+        childs: [1,2,3]
       }
     ]
-  };
+  });
+});
+
+router.get('/:id', isLoggedIn, function(req, res) {
+  var id = req.params.id;
   res.render('dashboard', object);
 });
 
